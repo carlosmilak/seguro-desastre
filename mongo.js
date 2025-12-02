f70d01135768e04b1f44324267b320d861c871b4
carlosmilak <129692257+carlosmilak@users.noreply.github.com>
2025-12-02 20:37:43 -0300

    Add MongoDB connection utility

---
+++ mongo.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Atlas conectado!");
+  } catch (err) {
+    console.error("❌ Erro ao conectar no MongoDB:", err.message);
+    process.exit(1);
+  }
+};
